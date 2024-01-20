a3b70e18ddbd3eeaf2e4883054ac686c2171d59a
Doyun_hwang <materokatti@gmail.com>
2024-01-20 10:29:21 +0900

    feature : 404.html, style.css, index.html

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const { createServer } = require("http");
+const { createReadStream } = require("fs");
+
+const sendFile = (res, status, type, file) => {
+  res.writeHead(status, { "Content-Type": type });
+  createReadStream(file).pipe(res);
+};
 
 createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-
-  res.end(`
-    <!doctype html>
-    <html>
-      <head>
-        <title>Hello World</title>
-      </head>
-      <body>
-        <h1>Hello World</h1>
-        <p>${req.method} request made for ${req.url}</p>
-      </body>
-    </html>
-  `);
+  switch (req.url) {
+    case "/":
+      return sendFile(res, 200, "text/html", "./index.html");
+    case "/index.js":
+      return sendFile(res, 200, "text/javascript", "./index.js");
+    case "/style.css":
+      return sendFile(res, 200, "text/css", "./css/style.css");
+    default:
+      return sendFile(res, 404, "text/html", "./404.html");
+  }
 }).listen(3000, () => console.log("Server running - 3000"));
