6f1949548273181347c1a3db0f43913ac1bf179c
Doyun_hwang <materokatti@gmail.com>
2024-01-23 17:04:46 +0900

    ADD : Google api for Get google sheet data

--- server.js
+++ server.js
@@ -2,9 +2,23 @@ import express from "express";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import { google } from "googleapis";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+process.env.GOOGLE_APPLICATION_CREDENTIALS =
+  process.env.GOOGLE_APPLICATION_CREDENTIALS || ".env/credential.json";
 
 const app = express();
 app.use(express.json());
+
+// Google Sheets API 인증
+const auth = new google.auth.GoogleAuth({
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
+
+const sheets = google.sheets({ version: "v4", auth });
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -33,8 +47,23 @@ app.get("/vocaData", (req, res) => {
   res.send(vocaData);
 });
 
+// Get : Google Sheets vocaData
+app.get("/sheetData", async (req, res) => {
+  const client = await auth.getClient();
+  const sheets = google.sheets({ version: "v4", auth: client });
+  try {
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId: "1b3az54K2-P1BEB0IVdQ4Ces9a3NHsrlKqtxzeJuJnU0",
+      range: "test!A:B",
+    });
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, "client", "404.html"));
 });
 
-app.listen(3000, () => console.log("Server running - 3000"));
+app.listen(8000, () => console.log("Server running - 8000"));
